93c3b0f52829eea79bdc17cd8d01b4340199406b
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-05-23 20:17:16 -0400

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Kiho Okazawa on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
